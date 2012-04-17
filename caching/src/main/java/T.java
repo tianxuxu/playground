2a4a3c00dda762a091e2ba356a5f71bef23850ec
Ralph Schaer <ralphschaer@gmail.com>
2012-04-17 09:31:56 +0200

    more fiddling with websocket,atmosphere

---
+++ caching/src/main/java/T.java
@@ -0,0 +1,11 @@
+
+public class T {
+
+	public static void main(String[] args) {
+		System.out.println(0x1.fffffffffffffp-2);
+		System.out.println(0x2p-2);
+		System.out.println(2e2);
+
+	}
+
+}
---
+++ ws-tomcat/src/main/java/com/hillert/atmosphere/HomeController.java
@@ -0,0 +1,83 @@
+package com.hillert.atmosphere;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+
+import org.atmosphere.cpr.Broadcaster;
+import org.atmosphere.cpr.BroadcasterFactory;
+import org.atmosphere.cpr.Meteor;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.social.twitter.api.SearchResults;
+import org.springframework.social.twitter.api.Tweet;
+import org.springframework.social.twitter.api.impl.TwitterTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.hillert.atmosphere.model.TwitterMessage;
+
+@Controller
+public class HomeController {
+
+	static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	@RequestMapping(value = "/websockets", method = RequestMethod.GET)
+	@ResponseBody
+	public void websockets(final Meteor m) {
+
+		//    	for (Broadcaster br : BroadcasterFactory.getDefault().lookupAll()) {
+		//			System.out.println(br);
+		//		}
+
+		final Broadcaster b = BroadcasterFactory.getDefault().lookup("/*");
+		m.setBroadcaster(b);
+		m.suspend(-1);
+
+
+		//        final HttpServletRequest  req = event.getRequest();
+		//        final HttpServletResponse res = event.getResponse();
+
+		final ObjectMapper mapper = new ObjectMapper();
+
+		//        event.suspend();
+		//
+		//        final Broadcaster bc = event.getBroadcaster();
+		//
+		
+		b.scheduleFixedBroadcast(new Callable<String>() {
+
+			private long sinceId = 0;
+
+			@Override
+			public String call() throws Exception {
+				final TwitterTemplate twitterTemplate = new TwitterTemplate();
+				final SearchResults results = twitterTemplate.searchOperations().search("world", 1, 5, sinceId, 0);
+
+				logger.info("sinceId: " + sinceId + "; maxId: " + results.getMaxId());
+
+				sinceId = results.getMaxId();
+
+				List<TwitterMessage> twitterMessages = new ArrayList<TwitterMessage>();
+
+				for (Tweet tweet : results.getTweets()) {
+					twitterMessages.add(new TwitterMessage(tweet.getId(), tweet.getCreatedAt(), tweet.getText(), tweet
+							.getFromUser(), tweet.getProfileImageUrl()));
+				}
+
+				String s = mapper.writeValueAsString(twitterMessages);
+				return s;
+			}
+
+		}, 10, TimeUnit.SECONDS);
+
+		//bc.delayBroadcast("Underlying Response now suspended");
+
+	}
+
+}
