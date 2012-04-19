a1f38bf0420ad3ca4e246857acbe3508426c97bc
Ralph Schaer <ralphschaer@gmail.com>
2012-04-19 15:15:11 +0200

    some more playing around

--- eventbus/src/main/java/ch/rasc/pubsub/guava/SpringConfig.java
+++ eventbus/src/main/java/ch/rasc/pubsub/guava/SpringConfig.java
@@ -1,9 +1,12 @@
 package ch.rasc.pubsub.guava;
 
+import java.util.concurrent.Executors;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import com.google.common.eventbus.AsyncEventBus;
 import com.google.common.eventbus.EventBus;
 
 @Configuration
@@ -12,11 +15,11 @@ public class SpringConfig {
 
 	@Bean
 	public EventBus eventBus() {
-		EventBus eventBus = new EventBus();
-		return eventBus;
+//		EventBus eventBus = new EventBus();
+//		return eventBus;
 
-		//AsyncEventBus asyncEventBus = new AsyncEventBus(Executors.newFixedThreadPool(10));
-		//return asyncEventBus;
+		AsyncEventBus asyncEventBus = new AsyncEventBus(Executors.newFixedThreadPool(10));
+		return asyncEventBus;
 	}
 
 }
--- eventbus/src/main/java/ch/rasc/pubsub/guava/Subscriber.java
+++ eventbus/src/main/java/ch/rasc/pubsub/guava/Subscriber.java
@@ -1,8 +1,9 @@
 package ch.rasc.pubsub.guava;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.stereotype.Service;
 
-import com.google.common.eventbus.AllowConcurrentEvents;
 import com.google.common.eventbus.DeadEvent;
 import com.google.common.eventbus.Subscribe;
 
@@ -10,19 +11,21 @@ import com.google.common.eventbus.Subscribe;
 public class Subscriber {
 
 	@Subscribe
-	@AllowConcurrentEvents
 	public void handleMsgEvent(MsgEvent event) {
 		System.out.println("MsgEvent: " + event.getMessage());
 	}
 
 	@Subscribe
-	@AllowConcurrentEvents
 	public void handleSpecialMsgEvent(SpecialMsgEvent event) {
 		System.out.println("SpecialMsgEvent: " + event.getMessage() + " User: " + event.getUser());
+		try {
+			TimeUnit.SECONDS.sleep(5);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	@Subscribe
-	@AllowConcurrentEvents
 	public void handleDeadEvents(DeadEvent deadEvent) {
 		System.out.print("Event without a subscriber: ");
 		System.out.println(deadEvent.getEvent());
