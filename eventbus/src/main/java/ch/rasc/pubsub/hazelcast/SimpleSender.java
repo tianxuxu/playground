97ac618ffe0889d05244712d71e4e8c2d845675e
Ralph Schaer <ralphschaer@gmail.com>
2012-04-20 06:43:25 +0200

    added example with hazelcast client api

---
+++ eventbus/src/main/java/ch/rasc/pubsub/hazelcast/SimpleSender.java
@@ -0,0 +1,19 @@
+package ch.rasc.pubsub.hazelcast;
+
+import com.hazelcast.client.ClientConfig;
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.core.HazelcastInstance;
+
+public class SimpleSender {
+
+	public static void main(String[] args) {
+		
+		ClientConfig clientConfig = new ClientConfig();
+		clientConfig.addAddress("localhost");
+
+		HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);			
+		client.getTopic("my_topic").publish(new ShortMessageEvent("message from a simple hazelcast client"));
+
+	}
+
+}
