86aebea5839f64a55ce002cb72f1b47b65cae94c
Ralph Schaer <ralphschaer@gmail.com>
2012-06-26 05:00:22 +0200

    playing with the code

--- github/src/main/java/ch/rasc/github/Mylyn.java
+++ github/src/main/java/ch/rasc/github/Mylyn.java
@@ -1,60 +1,66 @@
 package ch.rasc.github;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collection;
 
 import org.eclipse.egit.github.core.service.RepositoryService;
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.CanceledException;
-import org.eclipse.jgit.api.errors.DetachedHeadException;
-import org.eclipse.jgit.api.errors.InvalidConfigurationException;
-import org.eclipse.jgit.api.errors.InvalidRemoteException;
-import org.eclipse.jgit.api.errors.JGitInternalException;
-import org.eclipse.jgit.api.errors.NoHeadException;
-import org.eclipse.jgit.api.errors.RefNotFoundException;
-import org.eclipse.jgit.api.errors.WrongRepositoryStateException;
-import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.transport.RefSpec;
-import org.eclipse.jgit.transport.RemoteConfig;
-import org.eclipse.jgit.transport.URIish;
+import org.eclipse.jgit.transport.TrackingRefUpdate;
 
 public class Mylyn {
 
-	public static void main(String[] args) throws IOException, JGitInternalException, InvalidRemoteException, URISyntaxException, WrongRepositoryStateException, InvalidConfigurationException, DetachedHeadException, CanceledException, RefNotFoundException, NoHeadException {
+	public static void main(String[] args) throws Exception {
+
+		Path backupDir = Paths.get("github_backup");
+		Files.createDirectories(backupDir);
+
 		RepositoryService service = new RepositoryService();
 		for (org.eclipse.egit.github.core.Repository repo : service.getRepositories("ralscha")) {
-			System.out.println(repo.getName());
-			System.out.println(repo.getGitUrl());
-			System.out.println();
+
+			Path repoDir = backupDir.resolve(repo.getName());
+			Files.createDirectories(repoDir);
+
+			Path configFile = repoDir.resolve("config");
+			if (Files.exists(configFile)) {
+				System.out.println("fetching : " + repo.getName());
+				Collection<TrackingRefUpdate> updates = Git.open(repoDir.toFile()).fetch().call()
+						.getTrackingRefUpdates();
+				for (TrackingRefUpdate update : updates) {
+					System.out.println(update.getNewObjectId());
+					System.out.println(update.getOldObjectId());
+					System.out.println(update.getResult());
+				}
+			} else {
+				System.out.println("cloning : " + repo.getName());
+				Git.cloneRepository().setBare(true).setURI(repo.getGitUrl()).setDirectory(repoDir.toFile()).call();
+			}
+
 		}
 
-//		Git.cloneRepository().setBare(true).setURI("https://github.com/ralscha/embeddedtc.git")
-//		.setDirectory(new File("test")).call();
-		
-		Git.open(new File("test")).fetch().call();
-		
-//		Repository newRepo = Git.init().setBare(true).setDirectory(new File("new")).call().getRepository();
-//		
-//		RemoteConfig config = new RemoteConfig(newRepo.getConfig(), Constants.DEFAULT_REMOTE_NAME);
-//		URIish u = new URIish("git://github.com/ralscha/embeddedtc.git");
-//		config.addURI(u);
-//		config.setMirror(true);
-//		
-//		RefSpec refSpec = new RefSpec();
-//		refSpec = refSpec.setForceUpdate(true);
-//		refSpec = refSpec.setSourceDestination("refs/heads/*","refs/remotes/origin/*");
-//		config.addFetchRefSpec(refSpec);
-//		
-//		config.update(newRepo.getConfig());		
-//		newRepo.getConfig().save();
-//		
-//		
-//		Git.wrap(newRepo).fetch().call();
-//
-//		newRepo.close();
-		
+		// Repository newRepo = Git.init().setBare(true).setDirectory(new
+		// File("new")).call().getRepository();
+		//
+		// RemoteConfig config = new RemoteConfig(newRepo.getConfig(),
+		// Constants.DEFAULT_REMOTE_NAME);
+		// URIish u = new URIish("git://github.com/ralscha/embeddedtc.git");
+		// config.addURI(u);
+		// config.setMirror(true);
+		//
+		// RefSpec refSpec = new RefSpec();
+		// refSpec = refSpec.setForceUpdate(true);
+		// refSpec =
+		// refSpec.setSourceDestination("refs/heads/*","refs/remotes/origin/*");
+		// config.addFetchRefSpec(refSpec);
+		//
+		// config.update(newRepo.getConfig());
+		// newRepo.getConfig().save();
+		//
+		//
+		// Git.wrap(newRepo).fetch().call();
+		//
+		// newRepo.close();
 
 	}
 
