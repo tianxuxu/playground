d05ecf2d82e246662f62843dcc0b644a0ba3e7d2
Ralph Schaer <ralphschaer@gmail.com>
2012-06-26 04:23:31 +0200

    playing around

--- github/src/main/java/ch/rasc/github/Mylyn.java
+++ github/src/main/java/ch/rasc/github/Mylyn.java
@@ -6,8 +6,14 @@ import java.net.URISyntaxException;
 
 import org.eclipse.egit.github.core.service.RepositoryService;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.CanceledException;
+import org.eclipse.jgit.api.errors.DetachedHeadException;
+import org.eclipse.jgit.api.errors.InvalidConfigurationException;
 import org.eclipse.jgit.api.errors.InvalidRemoteException;
 import org.eclipse.jgit.api.errors.JGitInternalException;
+import org.eclipse.jgit.api.errors.NoHeadException;
+import org.eclipse.jgit.api.errors.RefNotFoundException;
+import org.eclipse.jgit.api.errors.WrongRepositoryStateException;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.transport.RefSpec;
@@ -16,7 +22,7 @@ import org.eclipse.jgit.transport.URIish;
 
 public class Mylyn {
 
-	public static void main(String[] args) throws IOException, JGitInternalException, InvalidRemoteException, URISyntaxException {
+	public static void main(String[] args) throws IOException, JGitInternalException, InvalidRemoteException, URISyntaxException, WrongRepositoryStateException, InvalidConfigurationException, DetachedHeadException, CanceledException, RefNotFoundException, NoHeadException {
 		RepositoryService service = new RepositoryService();
 		for (org.eclipse.egit.github.core.Repository repo : service.getRepositories("ralscha")) {
 			System.out.println(repo.getName());
@@ -24,8 +30,10 @@ public class Mylyn {
 			System.out.println();
 		}
 
-		Git.cloneRepository().setBare(true).setURI("https://github.com/ralscha/embeddedtc.git")
-		.setDirectory(new File("test")).call();
+//		Git.cloneRepository().setBare(true).setURI("https://github.com/ralscha/embeddedtc.git")
+//		.setDirectory(new File("test")).call();
+		
+		Git.open(new File("test")).fetch().call();
 		
 //		Repository newRepo = Git.init().setBare(true).setDirectory(new File("new")).call().getRepository();
 //		
