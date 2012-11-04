d89b9afa416140c0c924b30b53c026993e3c077a
Ralph Schaer <ralphschaer@gmail.com>
2012-11-04 07:45:05 +0100

    Add resourcejar generator. Generates maven artifacts for extjs

---
+++ resources-gen/src/main/java/ch/rasc/resourcesgen/CopyTree.java
@@ -0,0 +1,62 @@
+package ch.rasc.resourcesgen;
+
+import java.io.IOException;
+import java.nio.file.FileSystemLoopException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class CopyTree implements FileVisitor<Path> {
+
+	private final Path from;
+
+	private final Path to;
+
+	
+	public CopyTree(Path from, Path to) {
+		this.from = from;
+		this.to = to;
+	}
+
+	@Override
+	public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+		return FileVisitResult.CONTINUE;
+	}
+
+	@Override
+	public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
+		Path newdir = to.resolve(from.relativize(dir));
+		try {
+			Files.copy(dir, newdir, StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES);
+		} catch (IOException e) {
+			System.err.println("Unable to create " + newdir + " [" + e + "]");
+			return FileVisitResult.SKIP_SUBTREE;
+		}
+		return FileVisitResult.CONTINUE;
+	}
+
+	@Override
+	public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+		try {
+			Files.copy(file, to.resolve(from.relativize(file)), StandardCopyOption.REPLACE_EXISTING,
+					StandardCopyOption.COPY_ATTRIBUTES);
+		} catch (IOException e) {
+			System.err.println("Unable to copy " + file + " [" + e + "]");
+		}
+		return FileVisitResult.CONTINUE;
+	}
+
+	@Override
+	public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+		if (exc instanceof FileSystemLoopException) {
+			System.err.println("Cycle was detected: " + file);
+		} else {
+			System.err.println("Error occurred, unable to copy:" + file + " [" + exc + "]");
+		}
+		return FileVisitResult.CONTINUE;
+	}
+
+}
---
+++ resources-gen/src/main/java/ch/rasc/resourcesgen/Main.java
@@ -0,0 +1,153 @@
+package ch.rasc.resourcesgen;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.Set;
+
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
+
+import org.yaml.snakeyaml.Yaml;
+
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+
+public class Main {
+
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		ArgumentParser parser = ArgumentParsers.newArgumentParser("Main").defaultHelp(true)
+				.description("Creates maven packages of resourcejars");
+		parser.addArgument("file").nargs(1).required(true).help("Specify the configuration file");
+		Namespace ns = null;
+		try {
+			ns = parser.parseArgs(args);
+			if (ns == null) {
+				parser.printHelp();
+				System.exit(1);
+				return;
+			}
+		} catch (ArgumentParserException e) {
+			parser.handleError(e);
+			System.exit(1);
+			return;
+		}
+
+		String configFile = ns.<String> getList("file").iterator().next();
+
+		MustacheFactory mf = new DefaultMustacheFactory();
+		Mustache mustache = null;
+		try (InputStream is = Main.class.getResourceAsStream("/pom.mustache");
+				InputStreamReader isr = new InputStreamReader(is)) {
+			try {
+				mustache = mf.compile(isr, "pom.xml");
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.exit(1);
+				return;
+			}
+		}
+		if (mustache == null) {
+			System.exit(1);
+			return;
+		}
+
+		Map<String, Object> config;
+		try (FileReader fr = new FileReader(configFile)) {
+			Yaml yaml = new Yaml();
+			config = (Map<String, Object>) yaml.load(fr);
+		}
+
+		Map<String, String> files = (Map<String, String>) config.get("files");
+		Map<String, String> mappings = (Map<String, String>) config.get("mappings");
+
+		String sourcedir = (String) config.get("sourcedir");
+		String version = (String) config.get("version");
+		String groupId = (String) config.get("groupId");
+		String baseartifactId = (String) config.get("baseartifactId");
+		String destdir = (String) config.get("destdir");
+
+		Path srcDirPath = Paths.get(sourcedir);
+
+		Map<String, Object> scopes = Maps.newHashMap();
+		scopes.put("version", version);
+		scopes.put("groupId", groupId);
+
+		Set<Path> projectDirs = Sets.newHashSet();
+		
+		for (Map.Entry<String, String> entry : files.entrySet()) {
+			String fileName = entry.getKey();
+			String artifactId = entry.getValue();
+			if (artifactId.equals("core")) {
+				artifactId = baseartifactId;
+			} else {
+				artifactId = baseartifactId + "-" + artifactId;
+			}
+
+			scopes.put("artifactId", artifactId);
+
+			String artifactIdVersion = artifactId + "-" + version;
+			
+			Path destDirPath = Paths.get(destdir, artifactIdVersion, "src/main/resources/META-INF/resources",
+					groupId, baseartifactId, version);
+			Files.createDirectories(destDirPath);
+
+			projectDirs.add(Paths.get(destdir, artifactIdVersion));
+			
+			Path pomFile = Paths.get(destdir, artifactIdVersion, "pom.xml");
+			try (FileWriter fw = new FileWriter(pomFile.toFile())) {
+				mustache.execute(fw, scopes);
+			}
+
+			Path srcPath = srcDirPath.resolve(fileName);
+
+			String mappedFileName = mappings.get(fileName);
+			if (mappedFileName == null) {
+				mappedFileName = fileName;
+			}
+
+			Path destPath = destDirPath.resolve(mappedFileName);
+
+			if (!Files.isDirectory(srcPath) && destPath.getNameCount() > 1) {
+				Files.createDirectories(destPath.subpath(0, destPath.getNameCount() - 1));
+			} else if (destPath.getNameCount() > 1) {
+				Files.createDirectories(destPath);
+			}
+
+			copy(srcPath, destPath);
+
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for (Path path : projectDirs) {
+			sb.append("cd ");
+			sb.append(path.toString());
+			sb.append("\n");
+			sb.append("mvn clean deploy");
+			sb.append("\n");
+			sb.append("\n");
+		}
+
+		Files.write(Paths.get("deploy.sh"), sb.toString().getBytes());
+		
+		
+
+	}
+
+	private static void copy(Path from, Path to) throws IOException {
+		Files.walkFileTree(from, new CopyTree(from, to));
+	}
+
+}
