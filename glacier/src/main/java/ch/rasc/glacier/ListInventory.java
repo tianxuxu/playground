a4b5ccef1058dc188c0dcad5a56d71933e8341fb
Ralph Schaer <ralphschaer@gmail.com>
2013-02-28 07:27:50 +0100

    Update

--- glacier/src/main/java/ch/rasc/glacier/ListInventory.java
+++ glacier/src/main/java/ch/rasc/glacier/ListInventory.java
@@ -1,6 +1,7 @@
 package ch.rasc.glacier;
 
 import java.io.File;
+import java.util.Date;
 import java.util.concurrent.ConcurrentNavigableMap;
 
 import org.mapdb.DB;
@@ -8,16 +9,15 @@ import org.mapdb.DBMaker;
 
 public class ListInventory {
 
-
 	public static void main(String[] args) {
 		DB db = DBMaker.newFileDB(new File("glacierdb")).closeOnJvmShutdown().asyncWriteDisable().make();
 		ConcurrentNavigableMap<Long, String> files = db.getTreeMap("glacier");
 
 		for (Long ts : files.keySet()) {
-			System.out.println(ts + " --> " + files.get(ts));
+			Date d = new Date(ts);
+			System.out.printf("%1$tY:%1$tm:%1$te %1$tH:%1$tM:%1$tS --> %2$s\n", d, files.get(ts));
 		}
-		
+
 		db.close();
 	}
-
 }
