5d0f4619fde466e7ea53bf5418eec62ebe0300be
Ralph Schaer <ralphschaer@gmail.com>
2013-04-11 16:10:59 +0200

    More playing with s3

--- s3/src/main/java/ch/rasc/s3/DeleteBucket.java
+++ s3/src/main/java/ch/rasc/s3/DeleteBucket.java
@@ -3,6 +3,9 @@ package ch.rasc.s3;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 public class DeleteBucket {
 
@@ -12,7 +15,22 @@ public class DeleteBucket {
 			AWSCredentials credentials = new BasicAWSCredentials(args[0], args[1]);
 			AmazonS3Client client = new AmazonS3Client(credentials);
 
-			client.deleteBucket(args[2]);
+			String bucketName = args[2];
+			
+			ListObjectsRequest listObjectsRequest = new ListObjectsRequest().withBucketName(bucketName);
+			ObjectListing objectListing;
+
+			do {
+				objectListing = client.listObjects(listObjectsRequest);
+				for (S3ObjectSummary summary : objectListing.getObjectSummaries()) {
+					client.deleteObject(bucketName, summary.getKey());
+				}
+
+				listObjectsRequest.setMarker(objectListing.getNextMarker());
+			} while (objectListing.isTruncated());
+			
+			
+			client.deleteBucket(bucketName);
 			System.out.println("Bucket deleted: " + args[2]);
 		} else {
 			System.out.println("java -jar s3backup.jar " + DeleteBucket.class.getName()
