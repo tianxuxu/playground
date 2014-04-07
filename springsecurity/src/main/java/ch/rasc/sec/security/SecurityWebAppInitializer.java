cf42092dd9395ae2c076a142341882afe5c4fdc7
Ralph Schaer <ralphschaer@gmail.com>
2014-04-07 14:10:09 +0200

    Move the springsecurity example into ralscha/springsecuritytotp Update libraries

--- springsecurity/src/main/java/ch/rasc/sec/security/SecurityWebAppInitializer.java
+++
@@ -1,7 +0,0 @@
-package ch.rasc.sec.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationInitializer {
-	// noting here
-}
