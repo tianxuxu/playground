c15a266386a9f2cedee1fc0a981307d0788cdf8c
Ralph Schaer <ralphschaer@gmail.com>
2014-08-08 07:54:21 +0200

    Cleanup. Remove commented out code

--- cors/src/main/java/ch/rasc/cors/TestController.java
+++ cors/src/main/java/ch/rasc/cors/TestController.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,23 +13,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class TestController {
 
-	// @RequestMapping(value = "/users", method = RequestMethod.OPTIONS)
-	// public void handleCorsPreFlight(HttpServletResponse response) {
-	// System.out.println("preflight");
-	// response.addHeader("Access-Control-Allow-Origin", "*");
-	// response.addHeader("Access-Control-Allow-Methods", "POST");
-	// response.addHeader("Access-Control-Allow-Headers", "Content-Type");
-	// response.addHeader("Access-Control-Max-Age", "86400"); // seconds
-	//
-	// }
-
 	@RequestMapping(value = "/users", method = RequestMethod.POST)
 	@ResponseBody
-	public List<User> getUsers(@RequestBody String postData, HttpServletResponse response) {
+	public List<User> getUsers(@RequestBody String postData) {
 
 		System.out.println(postData);
-
-		// response.addHeader("Access-Control-Allow-Origin", "*");
 
 		List<User> builder = new ArrayList<>();
 		builder.add(new User(1, "admin"));
