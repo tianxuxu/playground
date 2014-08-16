85fa2cc009983e318329fef616396555f7f65ff2
Ralph Schaer <ralphschaer@gmail.com>
2014-08-16 03:48:55 +0200

    Update libraries

--- springmongodb/src/main/java/ch/rasc/springmongodb/fulltext/Main.java
+++ springmongodb/src/main/java/ch/rasc/springmongodb/fulltext/Main.java
@@ -32,8 +32,8 @@ import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.convert.CustomConversions;
 import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.text.TextCriteria;
-import org.springframework.data.mongodb.core.query.text.TextQuery;
+import org.springframework.data.mongodb.core.query.TextCriteria;
+import org.springframework.data.mongodb.core.query.TextQuery;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.util.StringUtils;
 
