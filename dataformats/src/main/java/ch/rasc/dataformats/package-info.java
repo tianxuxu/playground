35f6fe4656ddabd813b245babeaedadedf259939
Ralph Schaer <ralphschaer@gmail.com>
2014-12-06 19:01:35 +0100

    Moved to https://github.com/ralscha/dataformat

--- dataformats/src/main/java/ch/rasc/dataformats/package-info.java
+++
@@ -1,9 +0,0 @@
-@XmlJavaTypeAdapters({ @XmlJavaTypeAdapter(type = LocalDate.class,
-		value = LocalDateAdapter.class), })
-package ch.rasc.dataformats;
-
-import java.time.LocalDate;
-
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapters;
-
