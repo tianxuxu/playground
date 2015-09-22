1af1fd8e12be80c8de0c1a9cecd8cf11ea9d7d32
Ralph Schaer <ralphschaer@gmail.com>
2015-09-22 20:38:08 +0200

    Fiddling with the new gridfs implementation

--- mongodb/src/main/java/ch/rasc/mongodb/gridfs/Big.java
+++ mongodb/src/main/java/ch/rasc/mongodb/gridfs/Big.java
@@ -1,13 +1,20 @@
 package ch.rasc.mongodb.gridfs;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-import com.mongodb.DB;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
 import com.mongodb.MongoClient;
 import com.mongodb.MongoException;
-import com.mongodb.gridfs.GridFS;
-import com.mongodb.gridfs.GridFSInputFile;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.gridfs.GridFSBucket;
+import com.mongodb.client.gridfs.GridFSBuckets;
+import com.mongodb.client.gridfs.model.GridFSUploadOptions;
 
 public class Big {
 
@@ -18,21 +25,44 @@ public class Big {
 	}
 
 	private static void doSomething(MongoClient mongo) throws IOException {
-		@SuppressWarnings("deprecation")
-		DB db = mongo.getDB("testdb");
+		MongoDatabase db = mongo.getDatabase("testdb");
 
-		GridFS gfs = new GridFS(db);
+		GridFSBucket gridFSBucket = GridFSBuckets.create(db);
+		
 
-		File currentDir = new File(".");
-		File[] files = currentDir.listFiles();
-		for (File file : files) {
+//		byte[] data = "Data to upload into GridFS".getBytes(StandardCharsets.UTF_8);
+//		GridFSUploadStream uploadStream = gridFSBucket.openUploadStream("sampleData", options);
+//		uploadStream.write(data);
+//		uploadStream.close();
+//		System.out.println("The fileId of the uploaded file is: " + uploadStream.getFileId().toHexString());
+		
+		
+		Path currentDir = Paths.get(".");
+		
+		Files.list(currentDir).filter(Files::isRegularFile).forEach(p->{
+			
+			try {
+				String contentType = Files.probeContentType(p);
+				
+				try (InputStream is = Files.newInputStream(p)) {
 
-			if (file.isFile()) {
-
-				GridFSInputFile gfi = gfs.createFile(file);				
-				gfi.save();
+					GridFSUploadOptions options = new GridFSUploadOptions()
+					                                    .chunkSizeBytes(1024)
+					                                    .metadata(new Document("contentType", contentType));
+	
+					ObjectId fileId = gridFSBucket.uploadFromStream(p.getFileName().toString(), is, options);
+					System.out.println(p);
+					System.out.println(fileId);
+					System.out.println();
+				}
+				
 			}
-		}
+			catch (Exception e) {				
+				e.printStackTrace();
+			}
+			
+		});
+		
 
 	}
 
