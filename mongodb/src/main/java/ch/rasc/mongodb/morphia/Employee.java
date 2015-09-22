821af5cf8ba904a4131ff7c7a3e1b6c4ef368285
Ralph Schaer <ralphschaer@gmail.com>
2015-09-22 20:17:02 +0200

    Fiddling with morphia

---
+++ mongodb/src/main/java/ch/rasc/mongodb/morphia/Employee.java
@@ -0,0 +1,89 @@
+package ch.rasc.mongodb.morphia;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.types.ObjectId;
+import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Field;
+import org.mongodb.morphia.annotations.Id;
+import org.mongodb.morphia.annotations.Index;
+import org.mongodb.morphia.annotations.Indexes;
+import org.mongodb.morphia.annotations.Property;
+import org.mongodb.morphia.annotations.Reference;
+
+@Entity(noClassnameStored = true, value = "employees")
+@Indexes(@Index(fields = @Field("salary") ))
+class Employee {
+
+	@Id
+	private ObjectId id;
+
+	private String name;
+
+	@Reference
+	private Employee manager;
+
+	@Reference
+	private List<Employee> directReports = new ArrayList<>();
+
+	@Property("wage")
+	private Double salary;
+
+	public Employee() {
+		// default constructor
+	}
+
+	public Employee(String name, Double salary) {
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public ObjectId getId() {
+		return this.id;
+	}
+
+	public void setId(ObjectId id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Employee getManager() {
+		return this.manager;
+	}
+
+	public void setManager(Employee manager) {
+		this.manager = manager;
+	}
+
+	public List<Employee> getDirectReports() {
+		return this.directReports;
+	}
+
+	public void setDirectReports(List<Employee> directReports) {
+		this.directReports = directReports;
+	}
+
+	public Double getSalary() {
+		return this.salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + this.id + ", name=" + this.name + ", manager="
+				+ this.manager + ", directReports=" + this.directReports + ", salary="
+				+ this.salary + "]";
+	}
+
+}
---
+++ mongodb/src/main/java/ch/rasc/mongodb/morphia/Insert.java
@@ -0,0 +1,36 @@
+package ch.rasc.mongodb.morphia;
+
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.Morphia;
+
+import com.mongodb.MongoClient;
+
+public class Insert {
+
+	public static void main(String[] args) {
+		Morphia morphia = new Morphia();
+		morphia.mapPackage("ch.rasc.mongodb.morphia");
+
+		try (MongoClient mongoClient = new MongoClient()) {
+			mongoClient.getDatabase("morphia_example").getCollection("employees").drop();
+
+			Datastore datastore = morphia.createDatastore(mongoClient, "morphia_example");
+			datastore.ensureIndexes();
+
+			Employee elmer = new Employee("Elmer Fudd", 50000.0);
+
+			Employee daffy = new Employee("Daffy Duck", 40000.0);
+			datastore.save(daffy);
+
+			Employee pepe = new Employee("Pepé Le Pew", 25000.0);
+			datastore.save(pepe);
+
+			elmer.getDirectReports().add(daffy);
+			elmer.getDirectReports().add(pepe);
+
+			datastore.save(elmer);
+		}
+
+	}
+
+}
